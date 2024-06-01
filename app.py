800af7d102b07bf4109e3b6c268e1fda03002815
Henokh Abhinaya Tjahjadi <henokhtjahjadi@gmail.com>
2024-06-01 22:52:22 +0800

    add app.py and config.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from config import Config
+from models import db, User, Kegiatan, PersonalTask, Review, Catatan
+from flask_login import LoginManager
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
+    with app.app_context():
+        db.create_all()
+    
+    from routes import routes as routes_blueprint
+    app.register_blueprint(routes_blueprint)
+
+    return app
+
+from routes import *
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
